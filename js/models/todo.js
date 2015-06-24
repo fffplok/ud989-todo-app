e0baec8ee5711d27f4be4cbe6b9b2ddc52c6d287
fffplok <fffplok@gmail.com>
2015-06-24 13:59:22 -0500

    append -- done to completed label, add edit button

--- js/models/todo.js
+++ js/models/todo.js
@@ -19,8 +19,22 @@ var app = app || {};
 		// Toggle the `completed` state of this todo item.
 		toggle: function () {
 			this.save({
+				title: this.get('title'),
 				completed: !this.get('completed')
 			});
+
+/*		//not the best way to do this... should be done in the template... but works
+			var strDone = ' -- done',
+					strTitle = this.get('title'),
+					bCompleted = !this.get('completed');
+
+			strTitle = bCompleted ? strTitle + strDone : strTitle.slice(0, strTitle.indexOf(strDone));
+
+			this.save({
+				title: strTitle, //this.get('title')),
+				completed: bCompleted //!this.get('completed')
+			});
+*/
 		}
 	});
 })();
